2e4f72b2d7fd05e3da3f07aa1a1af39705baffb2
TangRufus <tangrufus@gmail.com>
2025-09-28 06:44:29 +0100

    wip

---
+++ internal/main_test.go
@@ -0,0 +1,15 @@
+package internal_test
+
+import (
+	"os/exec"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	p, err := exec.LookPath("composer-semver")
+	if err != nil {
+		t.Error("Could not find composer-semver binary in PATH")
+		t.FailNow()
+	}
+	t.Logf("Found composer-semver binary at %s", p)
+}
