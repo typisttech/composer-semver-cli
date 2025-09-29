92c7fbdcb33684360e21f533d1246f978124d60e
TangRufus <tangrufus@gmail.com>
2025-09-29 11:59:13 +0100

    wip

--- src/InfoCommand.php
+++ src/InfoCommand.php
@@ -27,6 +27,8 @@ class InfoCommand extends Command
                       |____/ \___|_| |_| |_|\_/ \___|_|
                       BANNER;
 
+    const BUILD_TIMESTAMP = '@datetime@';
+
     public function __construct(
         private readonly string $name,
         private readonly string $version,
@@ -42,35 +44,44 @@ class InfoCommand extends Command
         $io->newLine(2);
 
         $app = sprintf(
-            '%-15s <info>%s</info>',
+            '%-15s <info>%s</info> %s',
             $this->name,
             $this->version,
+            self::BUILD_TIMESTAMP,
         );
         $io->text($app);
 
-        $release = sprintf(
+        $githubUrl = sprintf(
             '<href=https://github.com/typisttech/composer-semver/releases/tag/%1$s>https://github.com/typisttech/composer-semver/releases/tag/%1$s</>',
             $this->version,
         );
         // https://github.com/box-project/box/blob/b0123f358f2a32488c92e09bf56f16d185e4e3cb/src/Configuration/Configuration.php#L2116
         if (preg_match('/^(?<tag>.+)-\d+-g(?<hash>[a-f0-9]{7})$/', $this->version, $matches)) {
             // Not on a tag.
-            $release = sprintf(
+            $githubUrl = sprintf(
                 '<href=https://github.com/typisttech/composer-semver/compare/%1$s...%2$s>https://github.com/typisttech/composer-semver/compare/%1$s...%2$s</>',
                 $matches['tag'],
                 $matches['hash'],
             );
         }
-        $io->text($release);
+        $io->text($githubUrl);
 
         $io->newLine(2);
         $io->writeln('<comment>Built with:</>');
+
+        $semVerVersion = InstalledVersions::getPrettyVersion('composer/semver');
         $semVer = sprintf(
-            '%1$-15s <href=https://github.com/composer/semver/releases/tag/%2$s>%2$s</>',
+            '%1$-15s %2$s',
             'composer/semver',
-            InstalledVersions::getPrettyVersion('composer/semver'),
+            $semVerVersion
         );
         $io->text($semVer);
+
+        $semVerReleaseUrl = sprintf(
+            '<href=https://github.com/composer/semver/releases/tag/%1$s>https://github.com/composer/semver/releases/tag/%1$s</>',
+            $semVerVersion
+        );
+        $io->text($semVerReleaseUrl);
 
         $io->newLine();
         $io->writeln('<comment>PHP:</>');
